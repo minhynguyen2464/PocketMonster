8f496db5b76eb12aa41d7adc00945c36f2a0a0b9
minhy <minhynguyen97@gmail.com>
2022-06-03 22:15:28 +0700

    Draw the map

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+//width & height of the canvas
+canvas.width = 1024;
+canvas.height = 576;
+
+//draw white rectangle
+c.fillStyle = 'white';
+c.fillRect(0,0,canvas.width,canvas.height); 
+
+//draw the map 
+const image = new Image();
+image.src = './img/Pellet Town.png';
+
+image.onload = () =>{
+    c.drawImage(image,-700,-550); //draw image method
+}
